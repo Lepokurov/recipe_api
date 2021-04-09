d37795eb9261ea1bbf13a235b75ceec71627eeb4
Andrey Lepokurov <endreyva@gmail.com>
2021-04-09 18:03:16 +0700

    Место так называемой сборки сервера

--- api/main.py
+++ api/main.py
@@ -1,57 +1,59 @@
+import json
+
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
 
-from models.recipes import get_recipes, get_recipe
+from api.items.auth import CurrentUser, authorization, login, register, logout
+from api.items.recipe_items import recipe_form, get_recipe, get_recipes_list, add_recipe, block_recipe, unblock_recipe, \
+    like_recipe, favorite_recipe, get_recipes_list_favorites, delete_recipe, change_recipe
+from api.items.user_items import user_form, block_user, unblock_user, upd_user, get_users_list, get_user, delete_user
 
 
-async def hello(request):
-    #data = await request.post()
-    #context = {}
-    #response = aiohttp_jinja2.render_template('recipe.html', request, context)
-    #a = get_recipe('Стейк')
-    return web.Response(text='OPPA')
-
-
-async def recipe_all(request):
-    """
-    Это минимальная форма поиска и добовления рецепта, для удобства
-    """
-    context = {}
-    response = aiohttp_jinja2.render_template('recipe.html', request, context)
+async def menu(request):
+    if CurrentUser.authorized or CurrentUser.admin:
+        context = {'nickname': CurrentUser.nickname}
+    else:
+        context = {'nickname': 'Not authorized'}
+    response = aiohttp_jinja2.render_template('index.html', request, context)
     return response
 
 
-async def recipes_data(request):
-    """
-    Возращает json список рецептов па запрашиваемым параметрам
-    """
-    data = await request.post()
-    recipes = get_recipes(data)
-    return web.Response(text=recipes)
+async def make_app() -> web.Application:
+    app = web.Application()
+
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.FileSystemLoader('templates')
+    )
+    # forms
+    app.add_routes([web.get('/authorization', authorization)])
+    app.add_routes([web.get('/recipe_form', recipe_form)])
+    app.add_routes([web.get('/user_form', user_form)])
+    app.add_routes([web.get('/', menu)])
+
+    app.add_routes([web.get('/login', login)])
+    app.add_routes([web.post('/registration', register)])
+    app.add_routes([web.get('/logout', logout)])
+
+    app.add_routes([web.put('/block_recipe', block_recipe)])
+    app.add_routes([web.put('/unblock_recipe', unblock_recipe)])
+    app.add_routes([web.put('/block_user', block_user)])
+    app.add_routes([web.put('/unblock_user', unblock_user)])
+
+    app.add_routes([web.get('/get_recipe', get_recipe)])
+    app.add_routes([web.get('/get_recipes_list', get_recipes_list)])
+    app.add_routes([web.get('/get_recipes_list_favorites', get_recipes_list_favorites)])
+    app.add_routes([web.post('/add_recipe', add_recipe)])
+    app.add_routes([web.post('/like_recipe', like_recipe)])
+    app.add_routes([web.post('/favorite_recipe', favorite_recipe)])
+    app.add_routes([web.put('/change_recipe', change_recipe)])
+    app.add_routes([web.delete('/delete_recipe', delete_recipe)])
+
+    app.add_routes([web.get('/get_user', get_user)])
+    app.add_routes([web.get('/get_users_list', get_users_list)])
+    app.add_routes([web.put('/update_user', upd_user)])
+    app.add_routes([web.delete('/delete_user', delete_user)])
+    return app
 
 
-async def recipe_data(request):
-    """
-    Возращает json рецепт по названию
-    (нужно по id, потому что названия могут повторяться, но пока для удобства отсавлю так)
-    """
-    data = await request.post()
-    recipe_ = get_recipe(data)
-    return web.Response(text=recipe_)
-
-
-# Тут просто мин проект с документации разростается чучуть.
-app = web.Application()
-
-aiohttp_jinja2.setup(
-    app, loader=jinja2.FileSystemLoader('templates')
-)
-# i added the styles, but it's just annoyed, and i turn them off
-#app.add_routes([web.static('/static', 'static')])
-app.add_routes([web.get('/', hello)])
-app.add_routes([web.get('/recipe_all', recipe_all)])
-app.add_routes([web.post('/recipe', recipes_data)])
-app.add_routes([web.post('/recipes', recipe_data)])
-
-web.run_app(app, host='127.0.0.1', port=5000)
+web.run_app(make_app(), host='127.0.0.1', port=5000)
